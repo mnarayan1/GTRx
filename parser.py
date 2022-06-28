c60f23c57c6f5f301710b07e25636111e32df844
mnarayan1 <madhumitas.narayan@gmail.com>
2022-06-27 23:40:50 -0700

    add functions, change file

--- parser.py
+++ parser.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import os
+import json
 
 
 def get_alternate_names(df, index):
-    # create data frame for condition names in each row
+    # create data frame for condition names in a row
     alternate_name_mask = df.columns.str.contains('condition_name_')
     alternate_name_df = df.iloc[:, alternate_name_mask]
     new_df = alternate_name_df.iloc[index].dropna()
@@ -21,7 +22,7 @@ def get_interventions(df, index):
     headers = ['int_description_', 'timeframe_int', 'age_use_int',
                'contra_int', 'qualscale_reclass_drug', 'rev1_eff_reclass_drug']
 
-    # create dataframe of all interventions for a particular row
+    # create dataframe of all interventions for a condition
     int_description_mask = df.columns.str.contains('int_description_')
     int_description_mask = df.iloc[:, int_description_mask]
     new_df = int_description_mask.iloc[index].dropna()
@@ -35,7 +36,7 @@ def get_interventions(df, index):
             if f'{header}{i}' in df.columns:
                 if pd.isnull(df[f'{header}{i}'][index]) == False:
                     doc[f'{header}{i}'] = df[f'{header}{i}'][index]
-        interventions_array.append(doc)
+        doc != {} and interventions_array.append(doc)
 
     return interventions_array
 
@@ -43,12 +44,12 @@ def get_interventions(df, index):
 def get_references(df, index):
     headers = ['pmid_title_', 'pmid_', 'pmid_date_', 'pmid_journal_']
 
-    # create data frame of all references for a particular row
+    # create data frame of all references for a condition
     references_mask = df.columns.str.contains('pmid_title_')
     references_df = df.iloc[:, references_mask]
     new_df = references_df.iloc[index].dropna()
 
-    # array of all references for a particular condition
+    # array of all references for a condition
     references_array = []
 
     for i in range(1, new_df.size+1):
@@ -57,34 +58,77 @@ def get_references(df, index):
             if f'{header}{i}' in df.columns:
                 if pd.isnull(df[f'{header}{i}'][index]) == False:
                     doc[f'{header}{i}'] = df[f'{header}{i}'][index]
-        references_array.append(doc)
+        doc != {} and references_array.append(doc)
 
     return references_array
 
 
+def get_clinical_summary(df, index):
+    headers = ['rcgim_clinical_summary', 'rcgim_clinical_summary2']
+
+    # array of clinical summaries for a condition
+    clinical_summary_array = []
+
+    for header in headers:
+        if header in df.columns:
+            if pd.isnull(df[header][index]) == False:
+                clinical_summary_array.append(df[header][index])
+
+    return clinical_summary_array
+
+
+def get_condition(df, index):
+    headers = ['condition_name', 'freq_per_birth', 'pattern_of_inheritance']
+
+    clinical_summary = get_clinical_summary(df, index)
+    alternate_names = get_alternate_names(df, index)
+
+    # condition information object
+    condition_information = {}
+
+    for header in headers:
+        if header in df.columns:
+            if pd.isnull(df[header][index]) == False:
+                condition_information[header] = df[header][index]
+
+    if clinical_summary != []:
+        condition_information['clinical_summary'] = clinical_summary
+    if alternate_names != []:
+        condition_information['alternate_names'] = alternate_names
+
+    return condition_information
+
+
+def get_gene_information(df, index):
+    headers = ['db_hgnc_gene_id', 'rcgim_clinical_summary2']
+
+    # object of gene information
+    gene_information = {}
+
+    for header in headers:
+        if header in df.columns:
+            if pd.isnull(df[header][index]) == False:
+                gene_information[header] = df[header][index]
+
+    return gene_information
+
+
 def load_data(data_folder):
     df = pd.read_excel(os.path.join(
-        data_folder, 'GTRx_Joined_Data2-1-2022.xlsx'), sheet_name='GTRx_Joined_Data2-1-2022')
+        data_folder, 'GTRx_Joined_Data2-1-2022.xlsx'), engine='openpyxl')
 
     for index, row in df.iterrows():
         id = row['record_id']
-        condition_name = row['condition_name']
-        clinical_summary = row['rcigm_clinical_summary']
-        freq_per_birth = row['freq_per_birth']
-        pattern_of_inheritance = row['pattern_of_inheritance']
-        alternate_names = get_alternate_names(df, index)
+        condition = get_condition(df, index)
+        gene_information = get_gene_information(df, index)
         interventions = get_interventions(df, index)
         references = get_references(df, index)
 
         doc = {}
         doc['_id'] = id
-        doc['condition'] = {
-            'condition_name': condition_name,
-            'alternate_names': alternate_names,
-            'freq_per_birth': freq_per_birth,
-            'pattern_of_inheritance': pattern_of_inheritance,
-        }
-        doc['clinical_summary'] = clinical_summary
+        doc['condition'] = condition
+        if gene_information != {}:
+            doc['gene information'] = gene_information
         doc['interventions'] = interventions
         doc['references'] = references
         yield doc
